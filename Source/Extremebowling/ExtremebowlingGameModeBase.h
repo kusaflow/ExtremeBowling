0f6ce240027e4f29ebb1b9915891637055f05588
Kunal SAgar <kunalsagar1999@gmail.com>
2020-09-10 13:38:14 +0530

    init

---
+++ Source/Extremebowling/ExtremebowlingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ExtremebowlingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EXTREMEBOWLING_API AExtremebowlingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
